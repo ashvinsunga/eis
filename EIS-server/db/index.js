2f98882a5f844bf11fe93d99f4a6c15a7584d6f0
Ashvin <ashvinsunga@yahoo.com>
2025-02-25 13:17:09 +0800

    connection restored

--- EIS-server/db/index.js
+++ EIS-server/db/index.js
@@ -1,8 +1,11 @@
 const { Pool } = require('pg');
 const { user, host, database, password, port} = require('../secrets/db_configuration')
+//connection restored
+
 
 const pool = new Pool({user, host, database, password, port});
 
 
 
 module.exports = pool;
+
